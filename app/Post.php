e11afe958a233f2363b934eeed60bf970e5d753f
qorbani moein <szmp.info@gmail.com>
2022-06-28 22:45:56 +0430

    test

---
+++ app/Post.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //asdasdasdasd
+    protected $table = 'post';
+    protected $fillable = ['title','content'];
+
+}
